6ef06b8dde7e8abe29427027ae9daafbb3301098
Thomas JEU <jeuthomas03@gmail.com>
2022-10-24 10:10:37 +0200

    setup base html twig

--- src/src/Controller/IndexController.php
+++ src/src/Controller/IndexController.php
@@ -10,7 +10,7 @@ class IndexController extends AbstractController
     #[Route('/', name: "app_index")]
     public function index()
     {
-        return $this->render("base.html.twig", [
+        return $this->render("index.html.twig", [
 
         ]);
     }
