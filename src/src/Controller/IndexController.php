9e7b1c635eb8ebccb0ec6ed36ad561275d5a295d
Thomas JEU <jeuthomas03@gmail.com>
2022-10-25 15:16:43 +0200

    [EDIT] les listes de produits

--- src/src/Controller/IndexController.php
+++ src/src/Controller/IndexController.php
@@ -11,8 +11,12 @@ class IndexController extends AbstractController
     #[Route('/', name: "app_index")]
     public function index(ProductRepository $repository)
     {
+        $list = $repository->findAll();
+        $filter6 = array_reverse($list);
+        $filter6reverse = array_slice($filter6, 0, 6);
+
         return $this->render("index.html.twig", [
-            'products' => $repository->findAll()
+            'products' => $filter6reverse,
         ]);
     }
 }
--- src/src/Controller/UserController.php
+++ src/src/Controller/UserController.php
@@ -11,8 +11,12 @@ class UserController extends AbstractController
     #[Route('/user/', name: "app_user")]
     public function user(ProductRepository $repository)
     {
+        $list = $repository->findAll();
+        $filter3 = array_reverse($list);
+        $filter3reverse = array_slice($filter3, 0, 3);
+
         return $this->render("user.html.twig", [
-            'products' => $repository->findAll()
+            'products' => $filter3reverse,
         ]);
     }
 }
