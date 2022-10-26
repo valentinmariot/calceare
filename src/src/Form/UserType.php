04c988d7beb1adc728e137b05b5808ef0b03820a
valentin Mariot <valentinmariot@gmail.com>
2022-10-26 22:19:00 +0200

    [ADD]: symfony/validator

---
+++ src/src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('email')
+            ->add('profile_picture')
+            ->add('rating')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
